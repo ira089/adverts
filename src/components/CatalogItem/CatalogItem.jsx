ebf049c6b2c5eaeed605d539af0323b73d7deb6c
ira089 <gvir2015@gmail.com>
2024-06-01 16:49:22 +0300

    catalogPage

--- src/components/CatalogItem/CatalogItem.jsx
+++ src/components/CatalogItem/CatalogItem.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-// import { CiLocationOn } from "react-icons/ci";
-// {/* <CiLocationOn /> */}
-// import { FaStar } from "react-icons/fa6";
-// {/* <FaStar /> */}
-// import { BsPeople } from "react-icons/bs";
-// <BsPeople />
-// import { TbAutomaticGearbox } from "react-icons/tb";
-// <TbAutomaticGearbox />
-// import { LuFuel } from "react-icons/lu"; petrol
-// <LuFuel />
+import Button from 'components/Button/Button';
+import styles from './catalogItem.module.css';
+import { FaRegHeart } from "react-icons/fa";
+import { CiLocationOn } from "react-icons/ci";
+import { FaStar } from "react-icons/fa6";
+import { BsPeople } from "react-icons/bs";
+import { TbAutomaticGearbox } from "react-icons/tb";
+import { LuFuel } from "react-icons/lu"; 
 // import { TbToolsKitchen2 } from "react-icons/tb";
 // <TbToolsKitchen2 />
 // import { IoBedOutline } from "react-icons/io5";
@@ -23,9 +21,59 @@ import React from 'react'
 
 
 
- const CatalogItem = (item) => {
+
+
+ const CatalogItem = ({item}) => {
+ const {adults, _id, details, gallery, description,
+  location, name, price, rating, reviews, transmission, engine} = item;
+ const image = gallery[0];
+//  console.log(gallery)
+ console.log(image);
+ console.log(details)
   return (
-    <li>CatalogItem</li>
+    <li key={_id} id={_id} className={styles.wrapItem}>
+       <img src={image} alt={name} className={styles.img}/>
+    <div className={styles.wrapText}>
+      {/* весь текст */}
+       <div className={styles.wrapTitle}>
+        {/* название цена локация отзывы */}
+            <div className={styles.title}>
+                <h2>{name}</h2> 
+                <div className={styles.wrapPrice}>
+                    <h2>{price}</h2>
+                    <button type='submit'>
+                    <FaRegHeart />
+                    </button>       
+                </div>
+
+            </div>          
+            <ul className={styles.wrapLocation}>
+                <li>
+                   <FaStar /> <span>{rating}({reviews.length} Reviews)</span>
+                </li>
+                <li>
+                    <CiLocationOn />  <span>{location}</span>
+                </li>
+            </ul>
+           
+        </div>
+        <p className={styles.wrapDescription}>{description}</p>
+        <ul className={styles.wrapEquipment}>
+            <li>
+                <BsPeople />  <span>{adults} adults</span>
+            </li>
+            <li>
+                 <TbAutomaticGearbox />  <span>{transmission}</span>
+            </li>
+            <li>
+                 <LuFuel />  <span>{engine}</span>
+            </li>
+        </ul>
+        <Button>Show more</Button>
+
+    </div>
+
+    </li>
   )
 }
 
--- src/components/CatalogList/CatalogList.jsx
+++ src/components/CatalogList/CatalogList.jsx
@@ -5,13 +5,13 @@ import * as camperSelectors from '../../redux/camperSelectors';
 import * as camperOperations from '../../redux/camperOperations';
 import styles from './CatalogList.module.css';
 import CatalogItem from '../CatalogItem/CatalogItem';
+import Button from 'components/Button/Button';
 
  const CatalogList = () => {
   const { isLoading, error, items } = useSelector(camperSelectors.selectCampers);
 
-console.log(isLoading)
-console.log(error)
 console.log(items)
+
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -29,11 +29,12 @@ console.log(items)
   const isItems = Boolean(items.length);
 
   return (
-    <>
+    <div>
     {isLoading && <p>...Loading</p>}
     {error && <p>{error.message}</p>}
     {isItems && <ul className={styles.list}>{elements}</ul>}
-  </>
+    <Button>Load more</Button>
+  </div>
     
   )
 }
