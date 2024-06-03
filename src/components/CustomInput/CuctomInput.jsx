a21aa30193f3fc551ec46be1e332cfa3d2304680
ira089 <gvir2015@gmail.com>
2024-06-03 18:16:01 +0300

    forma_upd

--- src/components/CustomInput/CuctomInput.jsx
+++ src/components/CustomInput/CuctomInput.jsx
@@ -3,9 +3,9 @@ import { LuCalendar } from "react-icons/lu";
 import styles from './customInput.module.css';
 
 const CustomInput = ({ value, onClick }) => (
-    <div  className={styles.customInput} onClick={onClick}>
+    <div  className={styles.customInput} >
       <input type="text" value={value}  placeholder="Booking date"  readOnly />
-      <LuCalendar size={20}/>
+      <LuCalendar onClick={onClick} size={20}/>
     </div>
   );
 
